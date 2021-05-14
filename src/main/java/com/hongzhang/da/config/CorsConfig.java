d93dba670756dafa7a661990905170fbb65dafc7
Walter.Xiang@honeywell.com <Walter.Xiang@honeywell.com>
2021-05-14 16:27:30 +0800

    add cros solution

---
+++ src/main/java/com/hongzhang/da/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.hongzhang.da.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Xiang Hongzhang
+ * @date 2021/05/14
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //允许跨域访问的路径
+        registry.addMapping("/**")
+        //允许跨域访问的源
+        .allowedOrigins("*")
+        //允许跨域请求的方法
+        .allowedMethods("POST","GET","PUT", "OPTIONS", "DELETE")
+        //预检间隔时间
+        .maxAge(168000)
+        //允许头部设置
+        .allowedHeaders("*")
+        //是否发送cookie
+        .allowCredentials(true);
+    }
+}
